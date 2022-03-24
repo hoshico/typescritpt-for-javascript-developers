f13a09baa1df47a4261cf20e267ad1ff077f4035
hoshico <melancholinista.yasu@gmail.com>
2022-03-25 07:42:56 +0900

    add string annotation

--- src/number-string.ts
+++ src/number-string.ts
@@ -1,9 +1,12 @@
 export {};
 
-let year = 1976;
-
+let year: number = 1976;
+// year = false;
 console.log(year);
 
-let age = 0x2b;
+let age: number = 0x2b;
 
 console.log(age);
+
+let name: string = "Ham";
+console.log(name);
