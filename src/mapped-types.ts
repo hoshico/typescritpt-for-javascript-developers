da7efef0def32cfca00368ae465c6cf71bc63719
hoshico <melancholinista.yasu@gmail.com>
2022-04-21 07:43:49 +0900

    fix mapped-types.ts

--- src/mapped-types.ts
+++ src/mapped-types.ts
@@ -6,3 +6,7 @@ type Profile = {
 };
 
 type PartilaProfile = Partial<Profile>;
+type PropertyTypes = keyof Profile;
+
+type Optional<T> = { [P in keyof T]?: T[P] | null };
+type OptionalProfile = Optional<Profile>;
