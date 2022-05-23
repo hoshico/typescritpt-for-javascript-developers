3be324d0e7584aeb1c8dfb4bd3d0baf993f5bbff
hoshico <melancholinista.yasu@gmail.com>
2022-05-24 08:20:04 +0900

    add types literal

--- src/review.ts
+++ src/review.ts
@@ -94,3 +94,13 @@ const ohtanishouhei: TwoWayPlayer = {
 // ユニオン型
 let value: number | string = 1;
 value = 'foo';
+
+//リテラル(文字/数字 より具体的な)
+// ＊ プリミティブ(単純/基本的な)
+
+let  dayOfTheWeek: '日'|'月'|'火'|'水'|'木'|'金'|'土' = '日';
+dayOfTheWeek = '月';
+
+let month: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 = 1;
+month = 12;
+
