c04f19e2fa4400164781fbf8b0887f5fd3b38665
hoshico <melancholinista.yasu@gmail.com>
2022-05-27 08:16:35 +0900

    add Person class

--- src/review-class.ts
+++ src/review-class.ts
@@ -1,4 +1,16 @@
 export {};
 
-class Person {}
-let taro = new Person();
+class Person {
+  name: string;
+  age: number;
+  // constructorは戻り値がないので、戻り値に対して型がいらない。
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  profile() {
+    return `name: ${this.name}, age: ${this.age}`
+  }
+}
+let taro = new Person('Taro', 30);
+console.log(taro.profile());
