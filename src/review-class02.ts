31897d18400d5a59ad16be3adc4bfa8d27f7f2c1
hoshico <melancholinista.yasu@gmail.com>
2022-05-29 13:46:51 +0900

    add MyNumberCard class

--- src/review-class02.ts
+++ src/review-class02.ts
@@ -7,3 +7,13 @@ class Person {
 
 const me = new Person('Ham', 30);
 console.log(me);
+
+class MyNumberCard {
+  owner: string;
+  secretNumber: number;
+
+  constructor(owner: string, secretNumber: number) {
+    this.owner = owner;
+    this.secretNumber = secretNumber;
+  }
+}
