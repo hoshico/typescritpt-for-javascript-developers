529652145df2326f8d6d34ab86eb6391f857d2a8
hoshico <melancholinista.yasu@gmail.com>
2022-05-31 08:24:26 +0900

    add review-readonly

---
+++ src/review-readonly.ts
@@ -0,0 +1,11 @@
+export {};
+
+// 初期化処理を簡略化する
+class VisaCard {
+  constructor(public readonly owner: string) {
+  }
+}
+
+let myVisaCard = new VisaCard('はむさん');
+console.log(myVisaCard.owner);
+myVisaCard.owner = 'ベーコン';
