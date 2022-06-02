8252529eba96f7e56c9ff410089d0f3f0474d8e7
hoshico <melancholinista.yasu@gmail.com>
2022-06-02 12:19:28 +0900

    ad Me class

--- src/review-static.ts
+++ src/review-static.ts
@@ -1,1 +1,20 @@
 export {}
+
+// 静的なメンバーの作成
+
+class Me {
+  static isProgrammer: boolean = true;
+  static firstName: string = 'Atushi';
+  static lastName: string = 'Ishida';
+
+  static work() {
+    //return "Hey, guys! Are you interested in TypeScript? let's dive into TypeScript!";
+    return `Hey, guys! This is ${this.firstName}! Are you interested in TypeScript? let's dive into TypeScript!`;
+  }
+}
+
+//let me = new Me();
+//console.log(me.isProgrammer);
+
+console.log(Me.isProgrammer);
+console.log(Me.work());
